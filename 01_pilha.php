91e75f1166d2aa589c90d4ea7dc9dd2a5f399b69
Weverton Matias <weverton-matias@hotmail.com>
2023-02-08 09:39:26 -0300

    Entendendo error e exception.

--- 01_pilha.php
+++ 01_pilha.php
@@ -3,6 +3,9 @@
 function func1()
 {
     echo 'Entrei função 1'.PHP_EOL;
+//    $arrayFixo = new SplFixedArray(2);
+//    $arrayFixo[3] = 'valor';
+//    $valor = intdiv(5, 0);
     func2();
     echo 'Saí função 1'.PHP_EOL;
 }
